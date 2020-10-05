10848b5f58c8afd52fafb8e1a8346fe93e9d2a06
Yicsson <yibsondavid@gmail.com>
2020-10-05 12:46:32 -0500

    Configurando servidor del backend

--- Backend_alquiler/Server/src/index.ts
+++ Backend_alquiler/Server/src/index.ts
@@ -1,1 +1,25 @@
-console.log('hello Word')
+import express, { Application } from 'express'
+
+class Server {
+    
+    public app: Application;
+    
+    constructor(){
+      this.app =  express()
+        
+    }
+
+    config(): void {
+        this.app.set('port',process.env.PORT || 3000)
+    }
+
+    routes(): void {
+
+    }
+
+    start():void {
+        this.app.listen(this.app.get('port'))
+    }
+}
+
+new Server()
